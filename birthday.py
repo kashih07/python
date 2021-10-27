a4fb4b4e0b58aff6f6360df602b4e43ecd1a30f7
harshith kashi <harshith.kv@sonata-software.com>
2021-10-27 22:30:06 +0530

    sec change

--- birthday.py
+++
@@ -1,8 +0,0 @@
-bdi={"Albert Einstein":"23/2/1542",
-"Benjamin Franklin":"22/12/7854",
-"Benjamin Franklin":"12/2/7854"}
-a=str(input("whos bday do u want??? "))
-if a in str(bdi.values()):
-    print(a)
-else:
-    print("ntg")
